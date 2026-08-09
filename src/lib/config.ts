dabc62ec744cd7ff13f2e8e8edebc7c05666ac9e
nikvn89 <nikkyvn89@gmail.com>
2026-08-09 19:36:02 +0700

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const CONTRACT_ADDRESS =
   (import.meta.env.VITE_CONTRACT_ADDRESS as `0x${string}` | undefined) ??
-  '0x7bf078785CB95Ac52FdcDaCf80b4Cc839e129C22'
+  '0xfd88Ffe790f90c361BB5bdEBC1A483a3d37699F5'
 
 export const STUDIO_RPC = 'https://studio.genlayer.com/api'
 export const EXPLORER_BASE = 'https://explorer-studio.genlayer.com'
